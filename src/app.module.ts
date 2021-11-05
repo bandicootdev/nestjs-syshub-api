5e741c748d77ffa719209389afab4d48c5d9d0ad
Thaymer Portillo <thaymerapv@gmail.com>
2021-11-04 20:45:48 -0400

    add integration with mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { StatsModule } from './stats/stats.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AddressModule } from './address/address.module';
 
 @Module({
   imports: [
@@ -8,7 +10,16 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          uri: config.get<string>('MONGO_URI'),
+        };
+      },
+    }),
     StatsModule,
+    AddressModule,
   ],
   controllers: [],
   providers: [],
